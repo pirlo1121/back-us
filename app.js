67085a7700dc589267f0885c1e80742cd3f3cbc4
Maicol bautista <pirlo1121s@gmail.com>
2024-04-21 21:43:16 -0500

    {+} mongo config

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { dbConection } = require('./src/config/mongo.config');
+require( 'dotenv' ).config();                
 
 const PORT = process.env.PORT || 3000
 
@@ -10,7 +12,7 @@ app.use( express.json());
 
 
 
-
+dbConection();
 app.listen( PORT, function() {
     console.log( `express funcionandoo en : ${ PORT }` );
 } );
