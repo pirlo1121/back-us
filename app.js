e775d4e90c8c256cd7e2c6627d29a3e9ec9c1a51
Maicol bautista <pirlo1121s@gmail.com>
2024-04-21 21:30:35 -0500

    {+} npm install express, dotenv, nodemon, cors. gitignore

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000
+
+
+app.use( cors());
+app.use( express.json());
+
+
+
+
+app.listen( PORT, function() {
+    console.log( `express funcionandoo en : ${ PORT }` );
+} );
