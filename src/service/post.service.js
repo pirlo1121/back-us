d2b53219b67246e4e2330bfdade345af7bbfd4b0
Maicol bautista <pirlo1121s@gmail.com>
2024-04-21 21:55:54 -0500

    {+} services Post

---
+++ src/service/post.service.js
@@ -0,0 +1,27 @@
+const postModel = require( '../model/post.model' );
+
+async function registerPost( post ) {
+    return await postModel.create( post );
+}
+
+async function getAllPosts() {
+    return await postModel.find();
+}
+
+async function getOnePostById( id ) {
+    return await postModel.findById( id );
+    //return await ProductModel.find({ _id: id });
+}
+
+async function removeOnePostById( id ) {
+    return await postModel.findOneAndRemove({ _id: id });
+}
+
+
+
+module.exports = {
+    registerPost, 
+    getAllPosts, 
+    getOnePostById,
+    removeOnePostById
+}
